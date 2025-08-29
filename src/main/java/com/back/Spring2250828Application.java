9e1e29409ff3797263e0336b379dca5ddd718d7a
dpwl0974 <dpwl0974@gmail.com>
2025-08-29 15:20:56 +0900

    refacotr & feat: 명언 게시판 구현

---
+++ src/main/java/com/back/Spring2250828Application.java
@@ -0,0 +1,15 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class Spring2250828Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2250828Application.class, args);
+    }
+
+}
